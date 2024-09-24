fe0c2663dba43c4f029687c42a5540f6a10bdf5a
amgg16 <124928492+amgg16@users.noreply.github.com>
2024-09-23 19:59:10 -0500

    Update gui11.py

--- build/gui11.py
+++ build/gui11.py
@@ -59,7 +59,7 @@ def quimico_ver_envio(frame3):
         400.0,
         24.0,
         anchor="nw",
-        text="BIENVENIDO",
+        text="Ver información de un envío",
         fill="#000000",
         font=("MicrosoftSansSerif", 32 * -1)
     )
